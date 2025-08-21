e2074e477a974b91a44bec6f1493253b955caf36
Akhil Varghese <124122740+akhil-pumex@users.noreply.github.com>
2025-08-21 12:47:29 +0530

    Project directory setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-green-700">In Development...</h1>
+    </div>
+  );
+}
+
+export default App;
